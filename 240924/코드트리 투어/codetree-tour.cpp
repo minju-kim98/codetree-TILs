7ab1fb1f1c711d334b5f4da2ac5b22f9680e936b
mj3meal <59169458+minju-kim98@users.noreply.github.com>
2024-09-24 19:46:53 +0900

    Title: 코드트리 투어, Time: 235ms, Memory: 2MB, Status: Passed - CodeTree

--- 240924/코드트리 투어/codetree-tour.cpp
+++ 240924/코드트리 투어/codetree-tour.cpp
@@ -28,17 +28,17 @@ struct Product {
 	}
 };
 
-vector<vector<Edge>> cities;
+vector<vector <Edge>> cities;
 priority_queue<Product> products;
 unordered_map<int, int> isValid;
 int N, M, start;
 vector<int> visited;
 
 void bfs() {
-	priority_queue<Edge> pq;
-	visited.assign(N, 1e9);  // 초기 값을 매우 큰 값으로 설정
-	visited[start] = 0;  // 시작점은 비용이 0
-	pq.push({start, 0});  // 우선순위 큐에 시작점 추가
+	priority_queue <Edge> pq;
+	visited.assign(N, 1e9);
+	visited[start] = 0;
+	pq.push({ start, 0 });
 
 	while (!pq.empty()) {
 		Edge nowEdge = pq.top();
@@ -49,14 +49,15 @@ void bfs() {
 		if (visited[now] < nowCost)
 			continue;
 
-		for (Edge nextEdge : cities[now]) {
+		for (int i = 0; i < cities[now].size(); i++) {
+			Edge nextEdge = cities[now][i];
 			int next = nextEdge.to;
 			int nextCost = nextEdge.value;
 
-			if (visited[next] > visited[now] + nextCost) {
-				visited[next] = visited[now] + nextCost;
-				pq.push({next, visited[next]});
-			}
+			if (visited[next] <= visited[now] + nextCost) continue;
+
+			visited[next] = visited[now] + nextCost;
+			pq.push({ next, visited[next] });
 		}
 	}
 }
@@ -68,55 +69,67 @@ int main() {
 		int inst;
 		cin >> inst;
 
-		if (inst == 100) {  // 도시와 도로 입력
+		if (inst == 100) {
 			cin >> N >> M;
 			cities.assign(N, vector<Edge>());
 			for (int i = 0; i < M; i++) {
 				int v, u, w;
 				cin >> v >> u >> w;
-				cities[v].push_back({u, w});
-				cities[u].push_back({v, w});
+				cities[v].push_back({ u, w });
+				cities[u].push_back({ v, w });
 			}
-			bfs();  // BFS를 통해 모든 도시의 최단 거리를 계산
+
+			bfs();
 		}
-		else if (inst == 200) {  // 새로운 제품 추가
+		else if (inst == 200) {
 			int id, revenue, dest, cost;
 			cin >> id >> revenue >> dest;
-			cost = visited[dest];  // 목적지까지의 최단 거리 비용 설정
-			products.push({id, revenue, dest, cost});
+			cost = visited[dest];
+			products.push({ id, revenue, dest, cost });
 			isValid[id] = 1;
 		}
-		else if (inst == 300) {  // 제품 무효화
+		else if (inst == 300) {
 			int id;
 			cin >> id;
 			if (isValid.find(id) != isValid.end()) {
-				isValid[id] = 0;  // 해당 제품 무효화
+				isValid[id] = 0;
 			}
 		}
-		else if (inst == 400) {  // 최적의 제품 선택
-			while (!products.empty() && isValid[products.top().id] == 0) {
-				products.pop();  // 무효화된 제품은 삭제
-			}
-
-			if (products.empty()) {
-				cout << -1 << endl;  // 유효한 제품이 없을 때
-			} else {
+		else if (inst == 400) {
+			if (products.empty()) cout << -1 << endl;
+			else {
 				int id = products.top().id;
 				int revenue = products.top().revenue;
 				int cost = products.top().cost;
 
-				// 비용이 너무 높아서 수익을 내지 못하는 경우 제거
-				if (cost == 1e9 || revenue < cost) {
-					cout << -1 << endl;
-				} else {
-					cout << id << endl;  // 최적의 제품 출력
+				while (!products.empty() && isValid[id] == 0) {
 					products.pop();
+					isValid.erase(id);
+					id = products.top().id;
+				}
+
+				if (products.empty()) {
+					cout << -1 << endl;
+				}
+				else {
+					id = products.top().id;
+					revenue = products.top().revenue;
+					cost = products.top().cost;
+
+					if (cost == 1e9 || revenue < cost) {
+						cout << -1 << endl;
+					}
+					else {
+						cout << id << endl;
+						products.pop();
+						isValid.erase(id);
+					}
 				}
 			}
 		}
-		else if (inst == 500) {  // 출발 도시 변경
+		else if (inst == 500) {
 			cin >> start;
-			bfs();  // 새로운 출발점으로 최단 거리 갱신
+			bfs();
 
 			priority_queue<Product> tmp;
 
@@ -125,14 +138,19 @@ int main() {
 
 				if (isValid[id] == 1) {
 					Product p = products.top();
-					p.cost = visited[p.dest];  // 비용 갱신
+					p.cost = visited[p.dest];
 					tmp.push(p);
+				}
+				else {
+					isValid.erase(id);
 				}
 				products.pop();
 			}
 
-			products = tmp;  // 갱신된 큐로 교체
+			products = tmp;
 		}
 	}
+
+
 	return 0;
 }
